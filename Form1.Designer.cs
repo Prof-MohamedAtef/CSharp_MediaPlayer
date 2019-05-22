185e92da9a160054b9258ea1bf8e2515d0149816
Prof-MohamedAtef <proofesser@gmail.com>
2019-05-22 03:26:31 -0700

    C# Windows Media Player

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace loopingsound_Good
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpenAudioFile = new System.Windows.Forms.Button();
+            this.playLoopingButton = new System.Windows.Forms.Button();
+            this.stopPlayingButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenAudioFile
+            // 
+            this.btnOpenAudioFile.Location = new System.Drawing.Point(12, 134);
+            this.btnOpenAudioFile.Name = "btnOpenAudioFile";
+            this.btnOpenAudioFile.Size = new System.Drawing.Size(163, 23);
+            this.btnOpenAudioFile.TabIndex = 1;
+            this.btnOpenAudioFile.Text = "Select a Sound File to repeat";
+            this.btnOpenAudioFile.UseVisualStyleBackColor = true;
+            this.btnOpenAudioFile.Click += new System.EventHandler(this.btnOpenAudioFile_Click);
+            // 
+            // playLoopingButton
+            // 
+            this.playLoopingButton.Location = new System.Drawing.Point(12, 29);
+            this.playLoopingButton.Name = "playLoopingButton";
+            this.playLoopingButton.Size = new System.Drawing.Size(75, 23);
+            this.playLoopingButton.TabIndex = 0;
+            this.playLoopingButton.Text = "Play";
+            this.playLoopingButton.UseVisualStyleBackColor = true;
+            this.playLoopingButton.Click += new System.EventHandler(this.playLoopingButton_Click);
+            // 
+            // stopPlayingButton
+            // 
+            this.stopPlayingButton.Location = new System.Drawing.Point(12, 68);
+            this.stopPlayingButton.Name = "stopPlayingButton";
+            this.stopPlayingButton.Size = new System.Drawing.Size(75, 23);
+            this.stopPlayingButton.TabIndex = 1;
+            this.stopPlayingButton.Text = "Stop";
+            this.stopPlayingButton.UseVisualStyleBackColor = true;
+            this.stopPlayingButton.Click += new System.EventHandler(this.stopPlayingButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(-1, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Prof-Mohamed - Mob : 2001097555350";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(-1, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "E_mail : proofesser@gmail.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(256, 218);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenAudioFile);
+            this.Controls.Add(this.stopPlayingButton);
+            this.Controls.Add(this.playLoopingButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Repeat Song";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stopPlayingButton;
+        private System.Windows.Forms.Button playLoopingButton;
+        private System.Windows.Forms.Button btnOpenAudioFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace loopingsound_Good
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer Player = new SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playLoopingButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Note: You may need to change the location specified based on
+                // the sounds loaded on your computer.
+                this.Player.SoundLocation = @"c:\users\mohamed\documents\visual studio 2010\Projects\loopingsound\loopingsound\_-_-_MP3_MP3.wav";
+                this.Player.PlayLooping();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error playing sound");
+            }
+        }
+
+        private void stopPlayingButton_Click(object sender, EventArgs e)
+        {
+            this.Player.Stop();
+        }
+
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private void btnOpenAudioFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((openFileDialog.OpenFile()) != null)
+                {
+                    string strfilename = openFileDialog.FileName;
+                    Player.SoundLocation = strfilename;
+                    Player.PlayLooping();
+
+                }
+            }
+            //timer1.Interval = 1;
+            //timer1.Tick += timer_Tick;
+            //timer1.Start();
+        }
+
+        
+        //void timer_Tick(object sender, EventArgs e)
+        //{
+        //    if ((openFileDialog.OpenFile()) != null)
+        //        label1.Text = String.Format("{0} / {1}", Player.LoadTimeout.ToString(@"mm\:ss"), Player.TimeSpan.ToString(@"mm\:ss"));
+        //    else
+        //        label1.Text = "No file selected...";
+        //}
+    }
+}
